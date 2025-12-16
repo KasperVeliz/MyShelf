f46ac6c2af540544f55dc806ef590bfd1d37ec03
Kasper <kasper.veliz@gmail.com>
2025-12-16 10:59:29 -0500

    Integrated book searching by title using openlibrary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,41 @@ import Shelf from './components/Shelf'
 import NavBar from './components/NavBar'
 
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [searchResult, setSearchResult] = useState([])
+
+  const searchBooks = async (search) => {
+    const filter = search.filter
+    const term = search.searchTerm
+    try{
+      if (filter == 'title'){
+        const response = await fetch(`https://openlibrary.org/search.json?q=${term}`)
+        let jsonData = await response.json()
+        setSearchResult(jsonData.docs.slice(0,5))
+        console.log(searchResult)
+      }
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  const handleSearchBooks = async(search) => {
+    searchBooks(search)
+  }
+
   return(
     <>
-      <NavBar />
-      <h1>My Shelf</h1>
-      <p>Your personal librarian</p>
-      <Shelf shelfName='Library'/>
-      <Shelf shelfName='Wishlist'/>
+      <NavBar onSearchBooks={handleSearchBooks}/>
+      <div>
+        <p>Your personal librarian</p>
+        <Shelf shelfName='Library'/>
+        <Shelf shelfName='Wishlist'/>
+      </div>
+      
     </>
   )
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,39 @@
+import { useState } from 'react'
+
 import './NavBar.css'
 
-function NavBar(){
-    
+function NavBar(props){
+
+    const [searchTerm, setSearchTerm] = useState('')
+    const [filter, setFilter] = useState('title')
+
+    const handleInput = (event) => {
+        setSearchTerm(event.target.value)
+    }
+
+    const handleFilter = (event) => {
+        setFilter(event.target.value)
+    }
+
+    return(
+        <div className='nav-bar'>
+            <h1>My Shelf</h1>
+            
+            <div className='search-bar-container'>
+                <form onSubmit={(event) => {
+                    event.preventDefault()
+                    props.onSearchBooks({filter,searchTerm})
+                }}>
+                    <select name='options' id='searchBy' onChange={handleFilter}>
+                        <option value='title'>Title</option>
+                        <option value='author'>Author</option>
+                    </select>
+                    <input className='search-bar' type='text' id='search-term' value={searchTerm} onChange={handleInput} placeholder='Search'/>
+                    <input id='submit' type='submit' value='Search'/>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default NavBar
